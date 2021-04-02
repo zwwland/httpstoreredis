60f006a9d9504ef284d1ff8f1b1eb446a42b6100
benchu1k <benchu1k@gmail.com>
2021-04-02 09:37:37 +0800

    v 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,18 @@ extern crate lazy_static;
 // use redis::{RedisResult, RedisError, Connection};
 
 extern crate deadpool_redis;
-
+extern crate json;
 
 use deadpool_redis::{cmd, Config, Pool};
-use deadpool_redis::redis::FromRedisValue;
+// use deadpool_redis::redis::FromRedisValue;
 
+use json::{object};
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
 // use std::sync::Mutex;
-use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
-
-
-
+use hyper::{body, Body, Method, Request, Response, Server, StatusCode};
 
 // rust 教学 https://www.runoob.com/rust/rust-tutorial.html
 // rust enum https://www.runoob.com/rust/rust-enum.html
@@ -34,13 +32,12 @@ use hyper::service::{make_service_fn, service_fn};
 // rust References(引用),borrowing(借用),&关键字,ref关键字,* https://www.jianshu.com/p/ac519d8c5ec9
 // STRUCT 1->struct Color (u8,u8,u8); 2->struct Color {r:u8,g:u8,b:u8}
 
-
 // 静态全局变量宏
 lazy_static! {
     #[derive(Debug)]
     static ref POOL:Pool = {
         let mut cfg = Config::default();
-        cfg.url  = Some("redis://127.0.0.1:6379/5".to_string());
+        cfg.url  = Some("redis://192.168.3.150:6379/5".to_string());
         let pool = cfg.create_pool().unwrap();
         // let client = redis::Client::open("redis://127.0.0.1:6379/5").unwrap();
         // let x = client.get_connection().unwrap();
@@ -51,30 +48,45 @@ lazy_static! {
 }
 // static Conn:redis::Connection;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn hello_world(mut _req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
 
-
-    match(_req.method(), _req.uri().path()) {
+    match (_req.method(), _req.uri().path()) {
         (&Method::GET, "/") => {
             println!("{:?}", &POOL);
             {
                 let mut conn = POOL.get().await.unwrap();
                 cmd("SET")
-                .arg(&["hello t", "199"])
-                .execute_async(&mut conn)
-                .await.unwrap();
+                    .arg(&["hello", "199"])
+                    .execute_async(&mut conn)
+                    .await
+                    .unwrap();
             }
             *response.body_mut() = Body::from("Hello World");
-        },
+        }
 
         (&Method::POST, "/getDeviceStatus") => {
+            {
+                let buffer = body::to_bytes(_req.body_mut()).await.unwrap();
+                let bd = String::from_utf8_lossy(&buffer);
+                let obj = json::parse(&bd).unwrap();
+                let ret = object! {
+                    name1: obj["name"].as_str(),
+                    time1: obj["time"].as_str()
+                };
 
+                let mut conn = POOL.get().await.unwrap();
+                cmd("SET")
+                    .arg(&["hello", &ret.dump()])
+                    .execute_async(&mut conn)
+                    .await
+                    .unwrap();
+            }
             *response.body_mut() = Body::from("getDeviceStatus");
-        },
+        }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-        },
+        }
     };
 
     Ok(response)
@@ -82,8 +94,7 @@ async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> 
 
 #[tokio::main]
 async fn main() {
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
         Ok::<_, Infallible>(service_fn(hello_world))
@@ -93,5 +104,4 @@ async fn main() {
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
-
 }
